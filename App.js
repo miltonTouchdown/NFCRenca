0200e99459fe129cbfe2750c4597ea96d3f7aa97
Milton Pulgar <miltonmja@hotmail.com>
2019-05-31 16:22:53 -0400

    + [IOS] reproduccion de audio al leer tag nfc + [Android] Abrir aplicacion al detectar tag (text/plain) y reproducir texto tag + [IOS] Agregado  key NFC info.plis

--- App.js
+++ App.js
@@ -31,6 +31,23 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const RtdType = {
+  URL: 0,
+  TEXT: 1,
+};
+
+function buildUrlPayload(valueToWrite) {
+  return Ndef.encodeMessage([
+      Ndef.uriRecord(valueToWrite),
+  ]);
+}
+
+function buildTextPayload(valueToWrite) {
+  return Ndef.encodeMessage([
+      Ndef.textRecord(valueToWrite),
+  ]);
+}
+
 type Props = {};
 export default class App extends Component<Props> 
 {
@@ -41,7 +58,14 @@ export default class App extends Component<Props>
     selectedVoice: null,
     speechRate: 0.5,
     speechPitch: 1,
-    text: "Esto es un texto de ejemplo. Lo logré"
+    text: "Esto es un texto de ejemplo. Lo logré",
+    NFC_supported: true,
+    NFC_enabled: false,
+    NFC_isWriting: false,
+    NFC_urlToWrite: 'https://www.google.com',
+    NFC_rtdType: RtdType.URL,
+    NFC_parsedText: null,
+    NFC_tag: {},
   };
 
   constructor(props) 
@@ -55,16 +79,28 @@ export default class App extends Component<Props>
       this.setState({ ttsStatus: "finished" })
     );
     Tts.addEventListener("tts-cancel", event =>
-      this.setState({ ttsStatus: "cancelled" })
+      this.setState({ ttsStatus: "cancelled: " + JSON.stringify(event)})
+      //utteranceid 10749950336
     );
     //Tts.setDefaultRate(this.state.speechRate);
     //Tts.setDefaultPitch(this.state.speechPitch);
     Tts.getInitStatus().then(this.initTts).then(this.getData).then(this._initConfig);
   }
 
-  componentDidMount()
-  {
-    //this.getData().then(this._initConfig);
+  componentDidMount() {
+    NfcManager.isSupported()
+        .then(NFC_supported => {
+            this.setState({ NFC_supported });
+            if (NFC_supported) {
+                this._startNfc();
+            }
+        })
+  }
+
+  componentWillUnmount() {
+      if (this._stateChangedSubscription) {
+          this._stateChangedSubscription.remove();
+      }
   }
 
   //#region TTS
@@ -128,6 +164,10 @@ export default class App extends Component<Props>
   readText = async () => {
     Tts.stop();
     Tts.speak(this.state.text);
+  };
+
+  stopText = async () => {
+    Tts.stop();
   };
 
   setSpeechRate = async rate => {
@@ -221,22 +261,283 @@ export default class App extends Component<Props>
 
   //#endregion
 
+  //#region NFC
+
+  _requestFormat = () => {
+    let {NFC_isWriting} = this.state;
+    if (NFC_isWriting) {
+        return;
+    }
+
+    this.setState({NFC_isWriting: true});
+    NfcManager.requestNdefWrite(null, {format: true})
+        .then(() => console.log('format completed'))
+        .catch(err => console.warn(err))
+        .then(() => this.setState({NFC_isWriting: false}));
+  }
+
+  _requestNdefWrite = () => {
+      let {NFC_isWriting, NFC_urlToWrite, NFC_rtdType} = this.state;
+      if (NFC_isWriting) {
+          return;
+      }
+
+      let bytes;
+
+      if (NFC_rtdType === RtdType.URL) {
+          bytes = buildUrlPayload(urlToWrite);
+      } else if (NFC_rtdType === RtdType.TEXT) {
+          bytes = buildTextPayload(NFC_urlToWrite);
+      }
+
+      this.setState({NFC_isWriting: true});
+      NfcManager.requestNdefWrite(bytes)
+          .then(() => console.log('write completed'))
+          .catch(err => console.warn(err))
+          .then(() => this.setState({NFC_isWriting: false}));
+  }
+
+  _cancelNdefWrite = () => {
+      this.setState({NFC_isWriting: false});
+      NfcManager.cancelNdefWrite()
+          .then(() => console.log('write cancelled'))
+          .catch(err => console.warn(err))
+  }
+
+  _requestAndroidBeam = () => {
+      let {NFC_isWriting, NFC_urlToWrite, NFC_rtdType} = this.state;
+      if (NFC_isWriting) {
+          return;
+      }
+
+      let bytes;
+
+      if (NFC_rtdType === RtdType.URL) {
+          bytes = buildUrlPayload(NFC_urlToWrite);
+      } else if (NFC_rtdType === RtdType.TEXT) {
+          bytes = buildTextPayload(NFC_urlToWrite);
+      }
+
+      this.setState({NFC_isWriting: true});
+      NfcManager.setNdefPushMessage(bytes)
+          .then(() => console.log('beam request completed'))
+          .catch(err => console.warn(err))
+  }
+
+  _cancelAndroidBeam = () => {
+      this.setState({NFC_isWriting: false});
+      NfcManager.setNdefPushMessage(null)
+          .then(() => console.log('beam cancelled'))
+          .catch(err => console.warn(err))
+  }
+
+  _startNfc() {
+      NfcManager.start({
+          onSessionClosedIOS: () => {
+              console.log('ios session closed');
+          }
+      })
+          .then(result => {
+              console.log('start OK', result);
+          })
+          .catch(error => {
+              console.warn('start fail', error);
+              this.setState({NFC_supported: false});
+          })
+
+      if (Platform.OS === 'android') {
+          NfcManager.getLaunchTagEvent()
+              .then(NFC_tag => {
+                  console.log('launch tag', NFC_tag);
+                  if (NFC_tag) 
+                  {
+                      /*
+                      * Se ha detectado un tag al iniciar la aplicacion
+                      */
+                      this.setState({ NFC_tag });
+                      let text = this._parseText(NFC_tag);
+                      this.setState({NFC_parsedText: text});
+
+                      //Iniciar lectura en audio del texto 
+                      this.setState({text: this._parseText(NFC_tag)});
+                      //Iniciar lectura en audio del texto      
+                      this._readNFCTagText();                    
+                  }
+              })
+              .catch(err => {
+                  console.log(err);
+              })
+          NfcManager.isEnabled()
+              .then(NFC_enabled => {
+                  this.setState({ NFC_enabled });
+              })
+              .catch(err => {
+                  console.log(err);
+              })
+          NfcManager.onStateChanged(
+              event => {
+                  if (event.state === 'on') {
+                      this.setState({NFC_enabled: true});
+                  } else if (event.state === 'off') {
+                      this.setState({enabled: false});
+                  } else if (event.state === 'turning_on') {
+                      // do whatever you want
+                  } else if (event.state === 'turning_off') {
+                      // do whatever you want
+                  }
+              }
+          )
+              .then(sub => {
+                  this._stateChangedSubscription = sub; 
+                  // remember to call this._stateChangedSubscription.remove()
+                  // when you don't want to listen to this anymore
+              })
+              .catch(err => {
+                  console.warn(err);
+              })
+      }
+  }
+
+  _onTagDiscovered = NFC_tag => {
+      console.log('Tag Discovered', NFC_tag);
+      //this.setState({NFC_tag });
+      // let url = this._parseUri(NFC_tag);
+      // if (url) {
+      //     Linking.openURL(url)
+      //         .catch(err => {
+      //             console.warn(err);
+      //         })
+      // }
+
+      //let text = this._parseText(NFC_tag);
+      //this.setState({NFC_parsedText: text});
+
+      /*
+      * Se ha detectado un tag al iniciar la aplicacion
+      */
+      this.setState({ NFC_tag });
+      let text = this._parseText(NFC_tag);
+      this.setState({NFC_parsedText: text});
+
+      this.setState({text: this._parseText(NFC_tag)});
+
+      if (Platform.OS === 'ios')
+      {
+        // Cerrar ventana de lectura nfc
+        // luego se espera un segundo para esperar el cierre
+        // de la interfaz de nfc para posteriormente 
+        //iniciar lectura en audio del texto 
+        NfcManager.unregisterTagEvent()
+          .then(result => {
+              console.log('unregisterTagEvent OK', result);   
+          })
+          .catch(error => {
+              console.warn('unregisterTagEvent fail', error)
+          }).then(setTimeout(this._readNFCTagText, 1000));
+
+      }else
+      {
+        // Android
+        //Iniciar lectura en audio del texto      
+        this._readNFCTagText();
+      }  
+  }
+
+  _startDetection = () => {
+    
+      NfcManager.registerTagEvent(
+        this._onTagDiscovered,
+        'Mantén tu dispositivo sobre el tag NFC',
+        {
+          //invalidateAfterFirstRead: true
+        }      
+        )
+          .then(result => {
+              console.log('registerTagEvent OK', result)
+          })
+          .catch(error => {
+              console.warn('registerTagEvent fail', error)
+          })
+  }
+
+  _stopDetection = () => {
+      NfcManager.unregisterTagEvent()
+          .then(result => {
+              console.log('unregisterTagEvent OK', result)             
+          })
+          .catch(error => {
+              console.warn('unregisterTagEvent fail', error)
+          })
+  }
+
+  _clearMessages = () => {
+      this.setState({NFC_tag: null});
+  }
+
+  _goToNfcSetting = () => {
+      if (Platform.OS === 'android') {
+          NfcManager.goToNfcSetting()
+              .then(result => {
+                  console.log('goToNfcSetting OK', result)
+              })
+              .catch(error => {
+                  console.warn('goToNfcSetting fail', error)
+              })
+      }
+  }
+
+  _parseUri = (NFC_tag) => {
+      try {
+          if (Ndef.isType(NFC_tag.ndefMessage[0], Ndef.TNF_WELL_KNOWN, Ndef.RTD_URI)) {
+              return Ndef.uri.decodePayload(NFC_tag.ndefMessage[0].payload);
+          }
+      } catch (e) {
+          console.log(e);
+      }
+      return null;
+  }
+
+  _parseText = (NFC_tag) => {
+      try {
+          if (Ndef.isType(NFC_tag.ndefMessage[0], Ndef.TNF_WELL_KNOWN, Ndef.RTD_TEXT)) {
+              return Ndef.text.decodePayload(NFC_tag.ndefMessage[0].payload);
+          }
+      } catch (e) {
+          console.log(e);
+      }
+      return null;
+  }
+
+  //#endregion
+
+  //#region Application_Renca
   _initConfig = async () =>
   {
     console.log("Init Config");
     Tts.setDefaultVoice(this.state.selectedVoice);
     Tts.setDefaultPitch(this.state.speechPitch);
     Tts.setDefaultRate(this.state.speechRate);
+
+    //this._startDetection();
   }
+
+  _readNFCTagText = async() => {
+    Tts.stop();
+    Tts.speak(this.state.text); 
+  };
+
+  //#endregion
 
   //#region Render
   render() 
   {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>{`React Native TTS Example`}</Text>
+        <Text style={styles.title}>{`NFC Renca`}</Text>
 
+        <Button title={`start nfc`} onPress={this._startDetection} />
         <Button title={`Read text`} onPress={this.readText} />
+        <Button title={`Stop text`} onPress={this.stopText} />
 
         <Text style={styles.label}>{`Status: ${this.state.ttsStatus ||
           ""}`}</Text>
